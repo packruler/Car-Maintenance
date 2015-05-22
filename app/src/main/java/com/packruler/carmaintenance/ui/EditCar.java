22f1460950299e8a513204e2ac40cb8fcc4a24f9
packruler <packruler@gmail.com>
2015-05-22 14:08:35 -0400

    New EditCar fragment work moving right along

--- app/src/main/java/com/packruler/carmaintenance/ui/EditCar.java
+++ app/src/main/java/com/packruler/carmaintenance/ui/EditCar.java
@@ -3,12 +3,14 @@ package com.packruler.carmaintenance.ui;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 import com.packruler.carmaintenance.R;
 import com.packruler.carmaintenance.sql.AvailableCarsSQL;
@@ -31,6 +33,7 @@ public class EditCar extends Fragment {
     private AvailableCarsSQL availableCarsSQL;
     private MainActivity activity;
 
+    private EditText nameText;
     private BetterSpinner yearSpinner;
     private BetterSpinner makeSpinner;
     private BetterSpinner modelSpinner;
@@ -61,14 +64,21 @@ public class EditCar extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_edit_car2, container, false);
+        initializeNameText();
         setYearSpinner();
         initializeMakeSpinner();
+        initizeModelSpinner();
 
         return rootView;
     }
 
+    private void initializeNameText() {
+        nameText = (EditText) rootView.findViewById(R.id.vehicle_name);
+        nameText.setInputType(InputType.TYPE_TEXT_VARIATION_SHORT_MESSAGE | InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+    }
+
     private void setYearSpinner() {
-        yearSpinner = (BetterSpinner) rootView.findViewById(R.id.year_spinner);
+        yearSpinner = (BetterSpinner) rootView.findViewById(R.id.year);
         List<String> yearList = new LinkedList<>();
 
         yearList.add(getString(R.string.other_selection));
@@ -77,22 +87,16 @@ public class EditCar extends Fragment {
         }
 
         yearSpinner.setAdapter(new ArrayAdapter<>(activity, android.R.layout.simple_list_item_1, yearList));
-        yearSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        yearSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             private final String TAG = "YearSpinnerItemClick";
 
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.v(TAG, "Selected: " + yearSpinner.getAdapter().getItem(position));
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if (yearSpinner.getAdapter().getItem(position).equals(getString(R.string.other_selection))) {
                     //TODO: Other selected
                 } else {
                     updateMakeSpinner();
                 }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
     }
@@ -100,21 +104,21 @@ public class EditCar extends Fragment {
     private List<String> makeList = new LinkedList<>();
 
     private void initializeMakeSpinner() {
-        makeList = new LinkedList<>();
         makeList.add(getString(R.string.other_selection));
 
-        makeSpinner = (BetterSpinner) rootView.findViewById(R.id.make_spinner);
+        makeSpinner = (BetterSpinner) rootView.findViewById(R.id.make);
         makeSpinner.setAdapter(new ArrayAdapter<>(activity, android.R.layout.simple_dropdown_item_1line, makeList));
-        makeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        makeSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             private final String TAG = "MakeSpinnerItemClick";
 
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.v(TAG, "Selected: " + yearSpinner.getAdapter().getItem(position));
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.v(TAG, "Selected: " + makeSpinner.getAdapter().getItem(position));
+                if (makeSpinner.getAdapter().getItem(position).equals(getString(R.string.other_selection))) {
+                    //TODO: Setup Other
+                } else {
+                    updateModelSpinner();
+                }
 
             }
         });
@@ -124,9 +128,42 @@ public class EditCar extends Fragment {
     private void updateMakeSpinner() {
         Log.v(TAG, "UpdateMakeSpinner");
         makeList.clear();
+        Log.v(TAG, "Year: " + yearSpinner.getText().toString());
+        makeList.addAll(availableCarsSQL.getAvailableMakes(yearSpinner.getText().toString()));
         makeList.add(getString(R.string.other_selection));
-        makeList.addAll(0, availableCarsSQL.getAvailableMakes(yearSpinner.getText().toString()));
 
         ((ArrayAdapter) makeSpinner.getAdapter()).notifyDataSetChanged();
+    }
+
+    private List<String> modelList = new LinkedList<>();
+
+    private void initizeModelSpinner() {
+        modelSpinner = (BetterSpinner) rootView.findViewById(R.id.model);
+        modelList.add(getString(R.string.other_selection));
+
+        modelSpinner.setAdapter(new ArrayAdapter<>(activity, android.R.layout.simple_list_item_1, modelList));
+        modelSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            private final String TAG = "MakeSpinnerItemClick";
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.v(TAG, "Selected: " + modelSpinner.getAdapter().getItem(position));
+                if (modelSpinner.getAdapter().getItem(position).equals(getString(R.string.other_selection))) {
+                    //TODO: Setup Other
+                } else {
+
+                }
+
+            }
+        });
+    }
+
+    private void updateModelSpinner() {
+        modelList.clear();
+
+        modelList.addAll(availableCarsSQL.getAvailableModels(yearSpinner.getText().toString(), makeSpinner.getText().toString()));
+        modelList.add(getString(R.string.other_selection));
+
+        ((ArrayAdapter) modelSpinner.getAdapter()).notifyDataSetChanged();
     }
 }
